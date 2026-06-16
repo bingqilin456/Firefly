14560abe5291f8f3583c86a0cb41af106cb0bacf
bingqilin456 <3566165112@qq.com>
2026-06-16 23:21:43 +0800

    111

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -1,18 +1,17 @@
 import type { ProfileConfig } from "../types/profileConfig";
-
 export const profileConfig: ProfileConfig = {
 	// 头像
 	// 图片路径支持三种格式：
 	// 1. public 目录（以 "/" 开头，不优化）："/assets/images/avatar.webp"
 	// 2. src 目录（不以 "/" 开头，自动优化但会增加构建时间，推荐）："assets/images/avatar.webp"
 	// 3. 远程 URL："https://example.com/avatar.jpg"
-	avatar: "assets/images/avatar.avif",
+	avatar: "assets/images/avatar.jpg",
 
 	// 名字
-	name: "Firefly",
+	name: "冰淇淋",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "哇哦，今天吃冰淇淋了吗？",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -20,12 +19,16 @@ export const profileConfig: ProfileConfig = {
 	// 如果想使用尚未包含相应的图标集，则需要安装它
 	// `pnpm add @iconify-json/<icon-set-name>`
 	// showName: true 时显示图标和名称，false 时只显示图标
+
+
 	links: [
 		{
+			// url: "http://localhost:4322/_image/qq-link.jpg",
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "assets/images/avatar.jpg",
 			showName: false,
+
 		},
 		{
 			name: "GitHub",
